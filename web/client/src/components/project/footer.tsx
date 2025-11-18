e2d42839c905082848eaf8e03e51c024a146d870
Cal <cydoval@mac.com>
2025-11-17 16:15:07 -0800

    footer centered

--- web/client/src/components/project/footer.tsx
+++ web/client/src/components/project/footer.tsx
@@ -2,23 +2,32 @@ import { Link } from '@tanstack/react-router';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-light-bg-200 border-t py-3 border-cru-border">
+    <footer className="bg-light-bg-200 border-t border-cru-border py-3">
       <div className="container flex items-center justify-between">
-        <div className="flex items-center gap-2">
+        {/* Left */}
+        <div className="flex-1 flex items-center">
           <h1 className="text-sm text-dark-font/70">Copyright 2025</h1>
         </div>
-        <img
-          alt="Dog outline logo"
-          className="w-8 opacity-70"
-          src="/walter.svg"
-        />
-        <Link to="https://caes.ucdavis.edu">
+
+        {/* Center */}
+        <div className="flex-1 flex justify-center">
           <img
-            alt="CA&ES UC Davis Logo"
-            className="w-36 opacity-70"
-            src="/caes.svg"
+            alt="Dog outline logo"
+            className="w-8 opacity-70"
+            src="/walter.svg"
           />
-        </Link>
+        </div>
+
+        {/* Right */}
+        <div className="flex-1 flex justify-end">
+          <Link to="https://caes.ucdavis.edu">
+            <img
+              alt="CA&ES UC Davis Logo"
+              className="w-36 opacity-70"
+              src="/caes.svg"
+            />
+          </Link>
+        </div>
       </div>
     </footer>
   );
