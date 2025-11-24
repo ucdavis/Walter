09338fb6645a37fd64f956a9d5518365729c6771
Scott Kirkland <srkirkland@gmail.com>
2025-11-24 13:34:07 -0800

    test and prod share same bicep template

--- web/deploy/production/run-deploy.sh
+++ web/deploy/production/run-deploy.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# This script deploys the Bicep template for the TEST infrastructure
+# This script deploys the Bicep template for the PRODUCTION infrastructure
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TEMPLATE_FILE="${SCRIPT_DIR}/../main.bicep"
 
-RESOURCE_GROUP="walter-test"
+RESOURCE_GROUP="walter"
 LOCATION="westus2"
-WEBAPP_NAME="walter-test"
-SQL_SERVER_NAME="walter-test"
-SQL_DB_NAME="walter-test"
+WEBAPP_NAME="walter"
+SQL_SERVER_NAME="walter"
+SQL_DB_NAME="walter"
+# TODO: Update APP_SERVICE_PLAN_ID for PRODUCTION
 APP_SERVICE_PLAN_ID="/subscriptions/105dede4-4731-492e-8c28-5121226319b0/resourceGroups/Default-Web-WestUS/providers/Microsoft.Web/serverfarms/DefaultPlan2"
 SQL_ADMIN_LOGIN="walter"
 SQL_ADMIN_PASSWORD="123456A!"
 
-# make sure you have the Azure CLI installed and logged in to the TEST subscription
+# make sure you have the Azure CLI installed and logged in to the PRODUCTION subscription
 
 # if you don't have the resource group, create it
 az group create -n "${RESOURCE_GROUP}" -l "${LOCATION}"
 
-# deploy the Bicep template with explicit TEST parameters
+# deploy the Bicep template with explicit PRODUCTION parameters
 az deployment group create \
   --resource-group "${RESOURCE_GROUP}" \
   --template-file "${TEMPLATE_FILE}" \
