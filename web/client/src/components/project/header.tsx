fae94dd6f3daac4e1d700afbf6399065e197cc85
Cal <cydoval@mac.com>
2026-01-14 11:28:27 -0800

    activity cards

--- web/client/src/components/project/header.tsx
+++ web/client/src/components/project/header.tsx
@@ -14,30 +14,26 @@ const formatAsOfDate = (value: string | null) => {
 const Header: React.FC = () => {
   const user = useUser();
   return (
-    <header className="bg-light-bg-200 border-b py-3 border-main-border sticky top-0 z-50">
+    <header className="bg-light-bg-200 border-b py-4 border-main-border sticky top-0 z-50">
       <div className="container flex items-center justify-between">
         <Link className="flex items-center gap-2" to="/">
-          <img alt="Dog outline logo" className="w-8" src="/walter.svg" />
-          <h1 className="text-2xl">Walter</h1>
+          <img alt="Dog outline logo" className="w-6" src="/walter.svg" />
+          <h1 className="text-xl">Walter</h1>
         </Link>
-        <div className="absolute top-[48px] left-1/2 -translate-x-1/2">
+        <div className="absolute top-[58px] left-1/2 -translate-x-1/2">
           <span className="inline-block bg-primary-color text-white px-3 py-1 text-xs rounded font-proxima-bold">
             DATA AS OF {formatAsOfDate(new Date().toDateString())}
           </span>
         </div>
-        <div className="flex items-center gap-8">
+        <div className="flex items-center">
           <nav className="flex items-center gap-6">
-            <Link to="/">Dashboard</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/personnel">Personnel</Link>
             <Link to="/reports">Reports</Link>
             <Link to="/accruals">Accruals</Link>
           </nav>
-          <div className="flex-col text-right ps-4">
-            <p className="font-proxima-bold">{user?.name}</p>
-          </div>
-          <div className="avatar">
-            <div className="w-8 rounded-full">
+          <div className="avatar ms-6">
+            <div className="w-10 rounded-full">
               <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
             </div>
           </div>
--- web/client/src/routes/(authenticated)/index.tsx
+++ web/client/src/routes/(authenticated)/index.tsx
@@ -1,3 +1,4 @@
+import CardsRow from '@/components/project/CardsRow.tsx';
 import { useUser } from '@/shared/auth/UserContext.tsx';
 import { createFileRoute } from '@tanstack/react-router';
 
@@ -7,5 +8,20 @@ export const Route = createFileRoute('/(authenticated)/')({
 
 function RouteComponent() {
   const user = useUser();
-  return <div className="container">home dash</div>;
+  return (
+    <div className="container">
+      home dash
+      <h2 className="h2 mt-8">Alerts</h2>
+      <div className="flex flex-col gap-4 mt-4">
+        <div className="alert alert-error alert-soft" role="alert">
+          <span>Error! Task failed successfully.</span>
+        </div>
+        <div className="alert alert-error alert-soft" role="alert">
+          <span>Error! Task failed successfully.</span>
+        </div>
+      </div>
+      <h2 className="h2 mt-8">Recently Viewed</h2>
+      <CardsRow />
+    </div>
+  );
 }
