880693796f0a1cc9af49f337da4b4eee1fb3e024
Cal <cydoval@mac.com>
2026-01-20 11:49:21 -0800

    font standardization

--- web/client/src/components/project/details.tsx
+++ web/client/src/components/project/details.tsx
@@ -20,51 +20,53 @@ export function ProjectDetails({ summary }: ProjectDetailsProps) {
       <div className="grid grid-cols-5 gap-4 mt-4">
         <div>
           <div className="h5">Start Date</div>
-          <div className="text-base">{formatDate(summary.awardStartDate)}</div>
+          <div className="h4">{formatDate(summary.awardStartDate)}</div>
         </div>
         <div>
           <div className="h5">End Date</div>
-          <div className="text-base">{formatDate(summary.awardEndDate)}</div>
+          <div className="h4">{formatDate(summary.awardEndDate)}</div>
         </div>
         <div>
           <div className="h5">PM</div>
-          <div className="text-base text-primary-color underline cursor-pointer">
+          <div className="h4 text-primary-color underline cursor-pointer">
             {displayValue(summary.pm)}
           </div>
         </div>
         <div>
           <div className="h5">PI</div>
-          <div className="text-base text-primary-color underline cursor-pointer">
+          <div className="h4 text-primary-color underline cursor-pointer">
             {displayValue(summary.pi)}
           </div>
         </div>
         <div>
           <div className="h5">ID</div>
-          <div className="text-base">{summary.projectNumber}</div>
+          <div className="h4">{summary.projectNumber}</div>
         </div>
       </div>
       <div className="grid grid-cols-5 gap-4 mt-6">
         <div>
           <div className="h5">Co-PI</div>
-          <div className="text-base">{displayValue(summary.copi)}</div>
+          <div className="h4">{displayValue(summary.copi)}</div>
         </div>
         <div>
           <div className="h5">PA</div>
-          <div className="text-base">{displayValue(summary.pa)}</div>
+          <div className="h4">{displayValue(summary.pa)}</div>
         </div>
         <div>
           <div className="h5">Status</div>
-          <div className="text-base">
-            {displayValue(summary.projectStatusCode)}
-          </div>
+          <div className="h4">{displayValue(summary.projectStatusCode)}</div>
         </div>
         <div>
           <div className="h5">Total Budget</div>
-          <Currency value={summary.totals.budget} />
+          <div className="h4">
+            <Currency value={summary.totals.budget} />
+          </div>
         </div>
         <div>
           <div className="h5">Balance</div>
-          <Currency value={summary.totals.balance} />
+          <div className="h4">
+            <Currency value={summary.totals.balance} />
+          </div>
         </div>
       </div>
     </section>
