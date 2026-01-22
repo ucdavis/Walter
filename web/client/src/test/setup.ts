9074d82c367afc57c364aee1f5ba99cf796d9077
Scott Kirkland <srkirkland@gmail.com>
2026-01-22 09:20:33 -0800

    path modals in tests

--- web/client/src/test/setup.ts
+++ web/client/src/test/setup.ts
@@ -1,1 +1,30 @@
 import '@testing-library/jest-dom/vitest';
+
+if (!('ResizeObserver' in window)) {
+  class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).ResizeObserver = ResizeObserver;
+}
+
+if (!Element.prototype.scrollIntoView) {
+  Element.prototype.scrollIntoView = function scrollIntoView() {};
+}
+
+if (typeof HTMLDialogElement !== 'undefined') {
+  if (!HTMLDialogElement.prototype.showModal) {
+    HTMLDialogElement.prototype.showModal = function showModal() {
+      this.setAttribute('open', '');
+    };
+  }
+
+  if (!HTMLDialogElement.prototype.close) {
+    HTMLDialogElement.prototype.close = function close() {
+      this.removeAttribute('open');
+    };
+  }
+}
