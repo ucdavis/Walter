d61fd92802fe9c631952fd088bfd49bb8b5e04c6
Cal <cydoval@mac.com>
2026-02-11 09:34:58 -0800

    test build fix

--- web/client/src/components/project/ProjectsSidebar.tsx
+++ web/client/src/components/project/ProjectsSidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { formatDate } from '@/lib/date.ts';
 import {
   projectsDetailQueryOptions,
