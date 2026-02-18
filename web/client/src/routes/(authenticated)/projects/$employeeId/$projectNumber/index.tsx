f5c7b22d14f95b967de1bd223f76f249553518a8
Cal <cydoval@mac.com>
2026-02-18 14:25:11 -0800

    shannon stat card changes

--- web/client/src/routes/(authenticated)/projects/$employeeId/$projectNumber/index.tsx
+++ web/client/src/routes/(authenticated)/projects/$employeeId/$projectNumber/index.tsx
@@ -50,7 +50,7 @@ function ProjectContent({
     <main className="flex-1">
       <section className="mt-8 mb-2">
         <h1 className="h1">{summary.displayName}</h1>
-        <h3 className="subtitle"> {summary.projectNumber}</h3>
+        <h3 className="subtitle"> Faculty Department Portfolio Report</h3>
         <div className="mt-6">
           <ProjectAlerts
             employeeId={employeeId}
