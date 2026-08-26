8084b5704e1ea208aa033353515f9331757c3bc2
Rob Martinsen <rmartinsen@ucdavis.edu>
2026-08-26 12:01:11 -0400

    Revert "Merge pull request #380 from ucdavis/rpm/cbr-rateset"
    This reverts commit 2a972a675e6a51eb77cde752ed24dbc8a66fdd8e, reversing
    changes made to 93f4f58cd419975070720f4052a3b25ea0d34c72.

--- datamart/Tables/CompositeBenefitRates.sql
+++ datamart/Tables/CompositeBenefitRates.sql
@@ -3,11 +3,10 @@ CREATE TABLE [dbo].[CompositeBenefitRates]
     [JobCode] VARCHAR(10) NOT NULL,
     [TitleCode] VARCHAR(10) NOT NULL,
     [Title] VARCHAR(100) NULL,
-    [PersonalPGMCode] VARCHAR(10) NULL,   -- UCD rows only
-    [TitleUnitCode] VARCHAR(10) NULL,     -- UCD rows only
+    [PersonalPGMCode] VARCHAR(10) NULL,
+    [TitleUnitCode] VARCHAR(10) NULL,
     [CBRGroup] VARCHAR(200) NULL,
-    [VacationAccrual] DECIMAL(5,4) NULL,  -- UCD rows only
+    [VacationAccrual] DECIMAL(5,4) NULL,
     [CBR] DECIMAL(5,4) NULL,
-    [RateSet] VARCHAR(10) NOT NULL CONSTRAINT [DF_CompositeBenefitRates_RateSet] DEFAULT 'UCD', -- 'UCD' (campus rates) or 'ANR' (federally approved rates); each set loaded out-of-band from its own workbook
-    CONSTRAINT [PK_CompositeBenefitRates] PRIMARY KEY ([RateSet], [JobCode])
+    CONSTRAINT [PK_CompositeBenefitRates] PRIMARY KEY ([JobCode])
 );
